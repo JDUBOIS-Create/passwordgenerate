5013c588c366b5004acd63b265be3574495032c5
JDUBOIS-Create <maildejulesdubois@gmail.com>
2024-09-30 15:44:17 +0200

    Update passwordgenerate.py

--- passwordgenerate.py
+++ passwordgenerate.py
@@ -2,16 +2,15 @@ import random
 import string
 
 def generer_mot_de_passe(taille=12):
-    # Définir les caractères possibles pour le mot de passe
+  
     caracteres = string.ascii_letters + string.digits + string.punctuation
     
-    # Générer un mot de passe aléatoire de la taille spécifiée
+  
     mot_de_passe = ''.join(random.choice(caracteres) for i in range(taille))
     
     return mot_de_passe
 
-# Exemple d'utilisation
 if __name__ == "__main__":
-    taille_mot_de_passe = 16  # Vous pouvez changer la taille du mot de passe ici
+    taille_mot_de_passe = 16 
     mot_de_passe = generer_mot_de_passe(taille_mot_de_passe)
     print("Mot de passe généré:", mot_de_passe)
